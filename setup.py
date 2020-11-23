5bb1a7822d1c7876109f9baf8d2092c08645bca2
Dan Borufka <danborufka@gmail.com>
2020-11-23 21:14:33 +0100

    Failsafing setup check

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python3
+
 import re
 import sys
 from pathlib import Path
 from utils.shell import shell_run
 
 def install_cli():
-	result = shell_run(['micro-components', 'get', 'version'])
+	try:
+		result = shell_run(['micro-components', 'get', 'version'])
+	except:
+		result = ''
 
 	if re.match(r'\w+ [\d\.]+', result):
 		print(' "micro-components" binary already installed. Skipping this part...')
